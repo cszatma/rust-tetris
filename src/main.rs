e30bf0c6d070656a66f35b8babe946f7a03eb78d
Christopher Szatmary <christopherszatmary@gmail.com>
2017-11-22 20:41:04 -0500

    Main function parses input into 2D vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::io::BufRead;
+mod game;
+mod tetrimino;
+
+use tetrimino::Tetrimino;
+
 fn main() {
-    println!("Hello, world!");
+    //let mut input = Vec::new();
+    let mut input:Vec<Vec<String>> = Vec::new();
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        //input.push(line.unwrap());
+        input.push(line.unwrap().split(' ').map(|s| s.to_string()).collect());
+    }
+    println!("{:?}", input);
 }
